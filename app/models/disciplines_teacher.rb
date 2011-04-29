23075051dabe78c34d06cbe9becffadc7d3d267c
Pavel Simonov <simonov_pavel89@mail.ru>
2011-04-29 21:34:57 +0400

    update_schema

--- app/models/disciplines_teacher.rb
+++ app/models/disciplines_teacher.rb
@@ -3,4 +3,5 @@ class DisciplinesTeacher < ActiveRecord::Base
   belongs_to :discipline, :foreign_key => 'discipline_id'
   validates_presence_of :teacher_id
   validates_presence_of :discipline_id
+  #validates_uniqueness_of :teacher_id, :scope => [:discipline_id]
 end
