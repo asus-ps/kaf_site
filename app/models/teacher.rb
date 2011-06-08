57bb0978ec882108d6b974093d2bf8f2d936d776
Pavel Simonov <simonov_pavel89@mail.ru>
2011-06-09 00:12:10 +0400

    Добавлена проверка на уникальность заведующего, выборка студентов по группам

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -12,6 +12,7 @@ class Teacher < ActiveRecord::Base
   accepts_nested_attributes_for :person
 
 
+  validate :validates_uniqueness_of_head_department
   
   def self.search(search,page)
     if search
@@ -20,6 +21,17 @@ class Teacher < ActiveRecord::Base
     else
       Teacher.all(:include => [:person, :degree, :position], :order => 'people.last_name').paginate(
         :per_page => 25, :page => page)
+    end
+  end
+
+private
+  def validates_uniqueness_of_head_department
+
+    if self.position_id == 1
+      c = Teacher.count(:conditions => ['position_id = ?', 1])
+      if c > 0
+        self.errors.add(:position_id, 'Заведующий на кафедре может быть только один!')
+      end
     end
   end
 
