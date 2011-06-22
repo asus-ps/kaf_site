d29e226179f5777c3a588a1a97af49c704dd01ca
kan <alexander.kostarev@gmail.com>
2011-06-22 12:15:21 +0400

    подготавливаемся к переносу на сайт

--- config/environment.rb
+++ config/environment.rb
@@ -46,10 +46,10 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'Europe/Moscow'  #'UTC'
-  config.gem 'russian'
-  config.gem 'will_paginate'
+#  config.gem 'russian'
+#  config.gem 'will_paginate'
 #  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-#  config.gem 'maruku'
+  config.gem 'maruku'
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
